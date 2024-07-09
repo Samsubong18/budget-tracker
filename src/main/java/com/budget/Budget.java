03977484e05f7c2fe4a01360c7863d6a8470e2f5
user <user@DESKTOP-SFISP9N>
2024-07-09 19:20:47 +0100

    Send Code

---
+++ src/main/java/com/budget/Budget.java
@@ -0,0 +1,29 @@
+package com.budget;
+
+import java.util.Date;
+
+public class Budget {
+    private Date date;
+    private double amount;
+
+    public Budget(Date date, double amount) {
+        this.date = date;
+        this.amount = amount;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+}
